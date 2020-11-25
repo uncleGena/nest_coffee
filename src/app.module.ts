7155f1a973f0e47bb5a9d626c5286a24b90d8a20
Andrew Kudriavtsev <mail.andrew.k@gmail.com>
2020-11-25 10:53:12 +0100

    load module async

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,6 +12,18 @@ import appConfig from './config/app.config';
 
 @Module({
   imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DATABASE_HOST,
+        port: +process.env.DATABASE_PORT,
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        autoLoadEntities: true,
+        synchronize: true, // disable on production
+      })
+    }), 
     ConfigModule.forRoot({
       load: [appConfig],
     }),
@@ -22,16 +34,6 @@ import appConfig from './config/app.config';
     //   })
     // }),
     CoffeesModule, 
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: +process.env.DATABASE_PORT,
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      autoLoadEntities: true,
-      synchronize: true, // disable on production
-    }), 
     // DatabaseModule
     CoffeeRatingModule
   ],
